185f002c4025e3143bf765cc14a4a4ff42c15c4b
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-08 22:22:35 +0300

    adding decimal and resolve reset value

--- script.js
+++ script.js
@@ -3,20 +3,33 @@ const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
 function sendNumberValue(number) {
-      // If  current display value is 0, replace it , if not add number
-      const displayValue = calculatorDisplay.textContent;
-      calculatorDisplay.textContent =  displayValue === '0' ? number :
-       displayValue + number;
+    // If current display value is 0, replace it , if not add number
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+}
+
+function addDecimal() {
+    // if no decimal, add one
+    if (!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
 }
 
 //Add event listeners for numbers, operators, decimal buttons
-
 inputBtns.forEach((inputBtn) => {
-       if(inputBtn.classList.length === 0) {
-          inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
-       } else if (inputBtn.classList.contains('operator')) {
-          inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))      
-       } else if (inputBtn.classList.contains('decimal')) {
-       inputBtn.addEventListener('click', () => sendNumberValue());
-       }
+    if (inputBtn.classList.length === 0) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('operator')) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('decimal')) {
+        inputBtn.addEventListener('click', () => addDecimal());
+    }
 });
+
+// Reset display
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+}
+
+//Event listener
+clearBtn.addEventListener('click', resetAll);

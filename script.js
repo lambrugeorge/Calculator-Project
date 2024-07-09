57c8ea505ae483a72ad26b4db35476b2a62d368a
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-09 17:53:06 +0300

    finish calculation function

--- script.js
+++ script.js
@@ -28,20 +28,39 @@ function addDecimal() {
     }
 }
 
+// Calculate first and second values depending on operator
+const calculate = {
+  '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+
+  '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+
+  '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+
+  '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+
+  '=': (firstNumber, secondNumber) => secondNumber,
+
+}
 
 const useOperator=(operator)=>{
        const currentValue = Number(calculatorDisplay.textContent);
+       // Prevent multiple operators
+
+       if (operatorValue && awaitingNextValue) {
+         operatorValue = operator; 
+           return
+       };
        //Assign firstValue if no value
        if (!firstValue) {
           firstValue = currentValue;
        } else {
-          console.log('currentValue', currentValue);
+          const calculation = calculate[operatorValue](firstValue, currentValue);
+          calculatorDisplay.textContent = calculation;
+          firstValue = calculation;
        }
        // Ready for next, store operator
        awaitingNextValue = true;
        operatorValue = operator;
-       console.log('firstValue', firstValue);
-       console.log('operator', operatorValue);
 }
 
 //Add event listeners for numbers, operators, decimal buttons
